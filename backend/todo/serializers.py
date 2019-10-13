961924e3d6bde1ff3a3f20d724258f23e7523210
Carter Gache <cartergache01@berkeley.edu>
2019-10-13 14:36:20 -0700

    Migration to Github

---
+++ backend/todo/serializers.py
@@ -0,0 +1,9 @@
+# convert model instances to JSON so that the frontend can work with the received data easily
+
+from rest_framework import serializers
+from .models import Todo
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todo
+        fields = ('id', 'title', 'description', 'completed')
